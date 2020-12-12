7530ae249493650c66de7cdba2a5c8f031da90d4
reza <reza.zafarpour1998@gmail.com>
2020-12-12 16:30:38 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+// const panels = document.querySelectorAll('.panel')
+
+// panels.forEach(panel => {
+//     panel.addEventlistener('click',() => {
+//         removeActiveClasses()
+//         panel.classList.add('active')
+//     })
+// })
+
+
+// function removeActiveClasses() {
+//     panels.forEach(panel => {
+//         panel.classList.remove('active')
+//     })
+// }
+
+
+const panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses()
+        panel.classList.add('active')
+    })
+})
+
+function removeActiveClasses() {
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
